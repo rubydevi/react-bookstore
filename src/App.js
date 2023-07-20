6f31bb3b92975b5c58b2e71298f433aebdbfdaaa
rubydevi <ariesabyss@gmail.com>
2023-07-20 21:15:52 +0530

    Import Navbar and Add Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
-}
+// App.js
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
+import React from 'react';
+import Navbar from './components/Navbar'; // Change the import statement to use a named import
+import Books from './components/Books';
+import Categories from './components/Categories';
+
+const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
