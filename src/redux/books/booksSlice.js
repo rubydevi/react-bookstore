f7e0098f9ae99f69d7f94edb1f4cda64e9374227
rubydevi <ariesabyss@gmail.com>
2023-07-24 14:23:39 +0530

    Set up reduxjs/toolkit and react-redux along with required files

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  books: [],
+};
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+
+  },
+});
