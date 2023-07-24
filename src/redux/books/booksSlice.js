35af87f11dcd4ebdf2968ad39edd96811f829f2d
rubydevi <ariesabyss@gmail.com>
2023-07-24 14:24:04 +0530

    Create addBook and removeBook actions

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -8,6 +8,14 @@ const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
-
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book.id !== action.payload);
+    },
   },
 });
+
+export const { addBook, removeBook } = booksSLice.actions;
+export default booksSlice.reducer;
