1fe23e07a0e9c7cf8b7280419e9d88291aa82910
rubydevi <ariesabyss@gmail.com>
2023-07-27 20:17:47 +0530

    Style Book component to match the given Zeplin template

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,9 +2,13 @@ import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import React from 'react';
 import Button from './Button';
+import CircularProgress from './CircularProgress';
 import { removeBookAsync } from '../redux/books/booksSlice';
+import '../styles/Book.css';
 
-const Book = ({ bookName, author, itemId }) => {
+const Book = ({
+  bookName, author, itemId, category,
+}) => {
   const dispatch = useDispatch();
 
   const handleRemoveBook = () => {
@@ -13,9 +17,23 @@ const Book = ({ bookName, author, itemId }) => {
 
   return (
     <li>
-      <h1>{bookName}</h1>
-      <p>{author}</p>
-      <Button label="Delete" onClick={handleRemoveBook} />
+      <section className="book">
+        <div className="book-content">
+          <div className="book-info">
+            <h4 className="book-category">{category}</h4>
+            <h2 className="book-title">{bookName}</h2>
+            <h6 className="book-author">{author}</h6>
+            <div className="buttons">
+              <button className="button" type="button">Comments</button>
+              <div className="divider" />
+              <Button className="button" label="Delete" onClick={handleRemoveBook} />
+              <div className="divider" />
+              <button className="button" type="button">Edit</button>
+            </div>
+          </div>
+          <CircularProgress />
+        </div>
+      </section>
     </li>
   );
 };
@@ -24,6 +42,7 @@ Book.propTypes = {
   bookName: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   itemId: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
 };
 
 export default Book;
